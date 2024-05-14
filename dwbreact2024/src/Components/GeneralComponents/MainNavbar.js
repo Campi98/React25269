12318cf0d967f6f551df969a835ae3705c21be5a
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-05-14 19:30:06 +0100

    tailwind removal completed
    complete tailwind remove from project

--- dwbreact2024/src/Components/GeneralComponents/MainNavbar.js
+++ dwbreact2024/src/Components/GeneralComponents/MainNavbar.js
@@ -8,7 +8,7 @@ function MainNavbar() {
             <div className="container px-4 px-lg-5">
                 <Link className="navbar-brand" to="/">NomeApp + Logo</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
+                    Menu&nbsp;
                     <i className="fas fa-bars"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
